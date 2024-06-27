f338a735c2a69183f391b1ead5e8fa039d10fc78
oscar1630 <164010163+Oscar-Moreno@users.noreply.github.com>
2024-06-27 12:01:15 +0200

    final css-card

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+
+const cardNumberElement = document.getElementById('card-number')
+const cardNameElement = document.getElementById('card-name')
+const cardDateElement = document.getElementById('card-date')
+const cardCvcElement = document.getElementById('card-cvc')
+
+const nameElement = document.getElementById('name')
+const numberElement = document.getElementById('name')
+const monthElement = document.getElementById('month')
+const yearElement = document.getAnimations('year')
+
+const enterName = event =>{
+    cardNameElement.textContent = nameElement.value 
+    if(cardNameElement.textContent === ''){
+        cardNameElement.value = 'JANE APLESSED'
+    }
+}
+nameElement.addEventListener('input',enterName)
