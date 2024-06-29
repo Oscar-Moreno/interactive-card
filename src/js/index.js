092eb6d2eed4e9184403f5267704be1ae3d7bfd4
oscar1630 <osmolomax@gmail.com>
2024-06-29 12:41:10 +0200

    final-tarjeta-sobreescribir

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,61 @@ import '../scss/styles.scss';
 
 const cardNumberElement = document.getElementById('card-number')
 const cardNameElement = document.getElementById('card-name')
-const cardDateElement = document.getElementById('card-date')
+const cardMonthElement = document.getElementById('card-month')
+const cardYearElement = document.getElementById('card-year')
 const cardCvcElement = document.getElementById('card-cvc')
 
 const nameElement = document.getElementById('name')
-const numberElement = document.getElementById('name')
+const numberElement = document.getElementById('number')
 const monthElement = document.getElementById('month')
-const yearElement = document.getAnimations('year')
+const yearElement = document.getElementById('year')
+const cvcElement = document.getElementById('cvc')
 
 const enterName = event =>{
     cardNameElement.textContent = nameElement.value 
     if(cardNameElement.textContent === ''){
-        cardNameElement.value = 'JANE APLESSED'
+        cardNameElement.textContent = 'JANE APLESSED'
     }
 }
-nameElement.addEventListener('input',enterName)
+nameElement.addEventListener('input', enterName)
+
+
+
+const enterNumber = event =>{
+    cardNumberElement.textContent = numberElement.value
+    if(cardNumberElement.textContent ===''){
+        cardNumberElement.textContent = '0000 0000 0000 0000'
+    }
+}
+numberElement.addEventListener('input', enterNumber)
+
+
+
+const enterMonth = event =>{
+    cardMonthElement.textContent = monthElement.value
+    if(cardMonthElement.textContent === ''){
+        cardMonthElement.textContent = ' 00'
+    }
+}
+monthElement.addEventListener('input', enterMonth)
+
+
+
+const enterYear = event =>{
+    cardYearElement.textContent = yearElement.value
+    if(cardYearElement.textContent === ''){
+        cardYearElement.textContent = ' 00'
+    }
+}
+yearElement.addEventListener('input', enterYear)
+
+
+
+
+const enterCvc = event =>{
+    cardCvcElement.textContent = cvcElement.value
+    if(cardCvcElement.textContent==''){
+        cardCvcElement.textContent = '000'
+    }
+}
+cvcElement.addEventListener('input', enterCvc)
